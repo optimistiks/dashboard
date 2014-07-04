ea75963e7b670c64eebe2e29d6b928ce8658a1fe
Potapov Max <mp@multiship.ru>
2014-07-04 18:58:56 +0400

    add mysql

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var sockjs = require('sockjs');
 var net = require('net');
+var mysql = require('mysql');
 
 var MS_HOST = '127.0.0.1';
 var MS_PORT = 8888;
@@ -9,7 +10,6 @@ var SJS_HOST = '0.0.0.0';
 var SJS_PORT = 9999;
 
 var connections = [];
-
 var chat = sockjs.createServer();
 chat.on('connection', function (conn) {
     connections.push(conn);
@@ -26,41 +26,60 @@ chat.on('connection', function (conn) {
         }
     });
 });
+var server = http.createServer();
+chat.installHandlers(server);
+server.listen(SJS_PORT, SJS_HOST);
+console.log('SockJS on ' + SJS_HOST + ':' + SJS_PORT);
 
 // Create a server instance, and chain the listen function to it
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
 // The sock object the callback function receives UNIQUE for each connection
 net.createServer(function (sock) {
-
     // We have a connection - a socket object is assigned to the connection automatically
     console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function (data) {
         console.log('DATA:', data.toString());
-
         for (var ii = 0; ii < connections.length; ii++) {
             connections[ii].write("MS says: " + data);
         }
     });
-
     // Add a 'close' event handler to this instance of socket
     sock.on('close', function (data) {
         console.log('CLOSED:', data);
     });
-
     //just added
     sock.on('error', function (err) {
         console.log('ERROR:', err);
     });
-
 }).listen(MS_PORT, MS_HOST);
-
 console.log('Listening on ' + MS_HOST + ':' + MS_PORT);
 
-var server = http.createServer();
-chat.installHandlers(server);
-server.listen(SJS_PORT, SJS_HOST);
-
-console.log('SockJS on ' + SJS_HOST + ':' + SJS_PORT);
+var orders = 0;
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'multiship',
+    password: 'multiship',
+    database: 'multiship'
+});
+connection.connect();
+var query = connection.query('SELECT * FROM sender_orders');
+query
+    .on('error', function (err) {
+        console.log('Error');
+        // Handle error, an 'end' event will be emitted after this as well
+    })
+    .on('fields', function (fields) {
+        console.log('Fields');
+        // the field packets for the rows to follow
+    })
+    .on('result', function (row) {
+        orders++;
+        // Pausing the connnection is useful if your processing involves I/O
+        console.log('Result')
+    })
+    .on('end', function () {
+        console.log('End', orders)
+    });
+connection.end();
 
