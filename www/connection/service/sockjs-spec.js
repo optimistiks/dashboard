1ad36e68f41e34a2f3e00acea894a3cb1217c302
Potapov Max <mp@multiship.ru>
2014-07-10 14:44:13 +0400

    add sample test

--- www/connection/service/sockjs-spec.js
+++ www/connection/service/sockjs-spec.js
@@ -3,7 +3,7 @@ describe('sockjs', function() {
   beforeEach(module('connection'));
 
   it('should ...', inject(function(sockjs) {
-
+    expect(1).toEqual(1);
 	//expect(sockjs.doSomething()).toEqual('something');
 
   }));
