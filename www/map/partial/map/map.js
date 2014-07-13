bf0318a7da67d71ed97f1e4a642640306bc0688f
Potapov Max <mp@multiship.ru>
2014-07-13 14:51:52 +0400

    draw points on map

--- www/map/partial/map/map.js
+++ www/map/partial/map/map.js
@@ -20,7 +20,10 @@ angular.module('map').controller('MapCtrl',function($scope){
 //        var color_domain = [50, 150, 350, 750, 1500];
 //        var color = d3.scale.threshold()
 //            .domain(color_domain)
-//            .range(["#adfcad", "#ffcb40", "#ffba00", "#ff7d73", "#ff4e40", "#ff1300"]);
+//            .range(["#adfcad", "#ffcb40", "#ffba00", "#ff7d73", "#ff4e40", "#ff1300"]);;
+
+        var colors = ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+            "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"];
 
         var ready = function (error, map, data) {
             console.log('MAP READY', error, map, data);
@@ -34,7 +37,33 @@ angular.module('map').controller('MapCtrl',function($scope){
                 .style("fill", function(d) {
                     return '#2b908f';
                 })
-                .style("opacity", 0.8)
+                .style("opacity", 0.8);
+
+            var city = svg.append("g")
+                .attr("class", "city")
+                .attr("transform", function(d) { return "translate(" + projection([37.9327, 55.796985]) + ")"; });
+
+            city.append("circle")
+                .attr("r", 3)
+                .style("fill", "white")
+                .style("opacity", 0.1);
+
+            city.append("text")
+                .attr("x", 5)
+                .text(function(d) { return 'Булошохы'; });
+
+            city = svg.append("g")
+                .attr("class", "city")
+                .attr("transform", function(d) { return "translate(" + projection([142.736913, 46.94379]) + ")"; });
+
+            city.append("circle")
+                .attr("r", 3)
+                .style("fill", "white")
+                .style("opacity", 0.1);
+
+            city.append("text")
+                .attr("x", 5)
+                .text(function(d) { return 'ЮжноСаха'; });
         };
 
         queue()
