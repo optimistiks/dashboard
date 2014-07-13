26bb7b87154e4c78024956d307a9cf06e638d62f
Potapov Max <mp@multiship.ru>
2014-07-13 13:49:56 +0400

    draw map

--- www/rootView/partial/index/index.js
+++ www/rootView/partial/index/index.js
@@ -32,5 +32,30 @@ angular.module('rootView').controller('IndexCtrl', function ($scope, sockjs) {
             .translate([width / 2, height / 2]);
 
         var path = d3.geo.path().projection(projection);
+
+        var color_domain = [50, 150, 350, 750, 1500]
+        var color = d3.scale.threshold()
+            .domain(color_domain)
+            .range(["#adfcad", "#ffcb40", "#ffba00", "#ff7d73", "#ff4e40", "#ff1300"]);
+
+        var ready = function (error, map, data) {
+            console.log('MAP READY', error, map, data);
+
+            svg.append("g")
+                .attr("class", "region")
+                .selectAll("path")
+                .data(topojson.feature(map, map.objects.russia).features)
+                .enter().append("path")
+                .attr("d", path)
+                .style("fill", function(d) {
+                    console.log('REGION', d.properties.region);
+                    return color(50);
+                })
+                .style("opacity", 0.8)
+        };
+
+        queue()
+            .defer(d3.json, "/rootView/partial/index/russia_1e-7sr.json")
+            .await(ready);
     });
 });
