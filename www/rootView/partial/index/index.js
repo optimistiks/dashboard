b31fe5e554dc4582351a18be945d5bdcadb8b891
Potapov Max <mp@multiship.ru>
2014-07-13 12:46:37 +0400

    add svg

--- www/rootView/partial/index/index.js
+++ www/rootView/partial/index/index.js
@@ -14,4 +14,23 @@ angular.module('rootView').controller('IndexCtrl', function ($scope, sockjs) {
         $scope.messages.push(event.data);
         console.log('SOCKET message', arguments);
     });
+
+    //d3
+    $(document).ready(function () {
+        var width = 960;
+        var height = 500;
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        var projection = d3.geo.albers()
+            .rotate([-105, 0])
+            .center([-10, 65])
+            .parallels([52, 64])
+            .scale(700)
+            .translate([width / 2, height / 2]);
+
+        var path = d3.geo.path().projection(projection);
+    });
 });
